6cb2e794e44a3c77ec10be24fe444a97f688cbde
Theo Pearson-Bray <tpb1908dev@gmail.com>
2018-10-06 21:10:18 +0100

    Add simple permissions request.

--- app/src/main/java/com/tpb/coinz/map/MapActivity.kt
+++ app/src/main/java/com/tpb/coinz/map/MapActivity.kt
@@ -6,31 +6,26 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
 import com.mapbox.android.core.location.LocationEngine
 import com.mapbox.android.core.location.LocationEngineListener
 import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.annotations.Marker
-import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.annotations.PolygonOptions
-import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.geometry.LatLngBounds
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
-import com.tpb.coinz.LocationListener
 import com.tpb.coinz.R
 import kotlinx.android.synthetic.main.activity_map.*
-import kotlinx.android.synthetic.main.activity_map.view.*
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), PermissionsListener {
 
     private lateinit var locationEngine: LocationEngine
     private lateinit var locationLayer: LocationLayerPlugin
+    private lateinit var permissionsManager: PermissionsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,38 +34,55 @@ class MapActivity : AppCompatActivity() {
 
         mapview.onCreate(savedInstanceState)
 
-
         moveToInitialLocation()
-        fab_coin_area.setOnClickListener(coinLocationOnClick)
-        fab_my_location.setOnClickListener(myLocationOnClick)
 
-        locationEngine = LocationEngineProvider(applicationContext).obtainBestLocationEngineAvailable()
-        locationEngine.activate()
-        locationEngine.addLocationEngineListener(object: LocationEngineListener {
-            override fun onLocationChanged(location: Location?) {
-                Log.i("LocationEngine", "Location update $location")
-            }
-
-            override fun onConnected() {
-                Log.i("LocationEngine", "Location engine connected")
-            }
-        })
-
-        mapview.getMapAsync {
-            locationLayer = LocationLayerPlugin(mapview, it, locationEngine)
-            locationLayer.renderMode = RenderMode.COMPASS
-            lifecycle.addObserver(locationLayer)
-        }
-
+        initLocationSystem()
     }
 
-    private fun checkLocation() {
+    private fun initLocationSystem() {
+        permissionsManager = PermissionsManager(this)
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
-            //TODO
+            fab_coin_area.setOnClickListener(coinLocationOnClick)
+            fab_my_location.setOnClickListener(myLocationOnClick)
+
+            locationEngine = LocationEngineProvider(applicationContext).obtainBestLocationEngineAvailable()
+            locationEngine.activate()
+            locationEngine.addLocationEngineListener(object: LocationEngineListener {
+                override fun onLocationChanged(location: Location?) {
+                    Log.i("LocationEngine", "Location update $location")
+                }
+
+                override fun onConnected() {
+                    Log.i("LocationEngine", "Location engine connected")
+                }
+            })
+
+            mapview.getMapAsync {
+                locationLayer = LocationLayerPlugin(mapview, it, locationEngine)
+                locationLayer.renderMode = RenderMode.GPS
+                lifecycle.addObserver(locationLayer)
+            }
+        } else {
+            permissionsManager.requestLocationPermissions(this)
         }
     }
 
-    private val myLocationOnClick: View.OnClickListener = View.OnClickListener {_ ->
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Log.i(MapActivity::class.java.name, "Permissions to explain $permissionsToExplain")
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        Log.i(MapActivity::class.java.name, "Permission result $granted")
+        initLocationSystem()
+    }
+
+    private val myLocationOnClick: View.OnClickListener = View.OnClickListener { _ ->
+        //TODO: First click for location, second for zoom
         mapview.getMapAsync {
             it.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(locationEngine.lastLocation), 15.0))
         }
