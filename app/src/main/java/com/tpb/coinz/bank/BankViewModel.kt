fba204c1106efb9485325c22dc3966d73989b197
Theo Pearson-Bray <tpb1908dev@gmail.com>
2018-11-16 13:33:45 +0000

    Add simple icons for coin types.

---
+++ app/src/main/java/com/tpb/coinz/bank/BankViewModel.kt
@@ -0,0 +1,11 @@
+package com.tpb.coinz.bank
+
+import android.app.Application
+import com.tpb.coinz.base.BaseViewModel
+
+class BankViewModel(application: Application) : BaseViewModel<BankNavigator>(application) {
+
+    override fun init() {
+
+    }
+}
