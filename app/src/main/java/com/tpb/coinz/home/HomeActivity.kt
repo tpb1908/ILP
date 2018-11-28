b7c524cf7d13b79f013a08c2968eb032d890c655
Theo Pearson-Bray <tpb1908dev@gmail.com>
2018-11-28 20:42:04 +0000

    Fix HomeActivity minimap LocationEngine.

--- app/src/main/java/com/tpb/coinz/home/HomeActivity.kt
+++ app/src/main/java/com/tpb/coinz/home/HomeActivity.kt
@@ -11,28 +11,36 @@ import androidx.lifecycle.ViewModelProviders
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseUser
-import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
 import com.tpb.coinz.App
 import com.tpb.coinz.BuildConfig
 import com.tpb.coinz.LocationUtils
 import com.tpb.coinz.R
+import com.tpb.coinz.data.location.LocationListeningEngine
+import com.tpb.coinz.data.location.LocationProvider
 import com.tpb.coinz.map.MapActivity
 import com.tpb.coinz.messaging.threads.ThreadsActivity
 import kotlinx.android.synthetic.main.activity_home.*
 import timber.log.Timber
+import javax.inject.Inject
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), PermissionsListener {
 
     private val rcLogin = 5534
     private lateinit var vm: HomeViewModel
 
+    @Inject
+    lateinit var locationProvider: LocationProvider
+    private lateinit var permissionsManager: PermissionsManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        (application as App).homeComponent.inject(this)
         home_minimap.onCreate(savedInstanceState)
 
         initViews(savedInstanceState)
@@ -43,10 +51,6 @@ class HomeActivity : AppCompatActivity() {
     private fun initViews(savedInstanceState: Bundle?) {
         home_minimap.onCreate(savedInstanceState)
         home_minimap.getMapAsync {
-            val locationLayer = LocationLayerPlugin(home_minimap, it, LocationEngineProvider(applicationContext).obtainBestLocationEngineAvailable())
-            locationLayer.renderMode = RenderMode.COMPASS
-            locationLayer.cameraMode = CameraMode.TRACKING_COMPASS
-            lifecycle.addObserver(locationLayer)
             it.animateCamera(CameraUpdateFactory.newLatLngBounds(LocationUtils.bounds, 10))
             it.addOnMapClickListener {latLng ->
                 val cameraPosition = it.cameraPosition
@@ -58,6 +62,24 @@ class HomeActivity : AppCompatActivity() {
                 intent.putExtra(getString(R.string.extra_camera_position), cameraPosition)
                 startActivity(intent, options.toBundle())
             }
+        }
+        initLocationSystem()
+    }
+
+    private fun initLocationSystem() {
+        locationProvider.start()
+        permissionsManager = PermissionsManager(this)
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            home_minimap.getMapAsync {
+                val engine = LocationListeningEngine(locationProvider)
+                val locationLayer = LocationLayerPlugin(home_minimap, it, engine)
+                locationLayer.renderMode = RenderMode.COMPASS
+                engine.activate()
+                lifecycle.addObserver(locationLayer)
+            }
+
+        } else {
+            permissionsManager.requestLocationPermissions(this)
         }
     }
 
@@ -112,5 +134,12 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Timber.i("Permissions to explain $permissionsToExplain")
+    }
 
+    override fun onPermissionResult(granted: Boolean) {
+        Timber.i("Permission result $granted")
+        initLocationSystem()
+    }
 }
